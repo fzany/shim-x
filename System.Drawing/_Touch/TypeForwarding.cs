4fe9f70a72d968098243e11d1fee2b1573032ad2
Anders Gustafsson <anders@cureos.com>
2014-01-13 22:49:53 +0100

    Use type forwarding to include duplicate classes in Touch library

---
+++ System.Drawing/_Touch/TypeForwarding.cs
@@ -0,0 +1,16 @@
+﻿//
+// Portability Class Library
+//
+// Copyright © Cureos AB, 2013
+// info at cureos dot com
+//
+
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+// System.Drawing
+[assembly: TypeForwardedTo(typeof(Point))]
+[assembly: TypeForwardedTo(typeof(PointF))]
+[assembly: TypeForwardedTo(typeof(Rectangle))]
+[assembly: TypeForwardedTo(typeof(Size))]
+[assembly: TypeForwardedTo(typeof(SizeF))]
