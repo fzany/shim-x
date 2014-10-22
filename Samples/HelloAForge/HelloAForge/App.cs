dee8033bc46e14f6bcccf709d7c603b6e2471263
Anders Gustafsson <anders@cureos.com>
2014-10-22 11:05:45 +0200

    Added Xamarin.Forms face detection project

---
+++ Samples/HelloAForge/HelloAForge/App.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2010-2014 Anders Gustafsson, Cureos AB.
+// All rights reserved. Any unauthorised reproduction of this 
+// material will constitute an infringement of copyright.
+
+namespace HelloAForge
+{
+    using Xamarin.Forms;
+
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new MainPage();
+        }
+    }
+}
