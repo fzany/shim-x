7e3939eb61a82c1c82cec2947854e7329bd4ab8d
Anders Gustafsson <anders@cureos.com>
2014-12-08 15:19:08 +0100

    Updated proprietary pixel format names

--- System.Drawing/_Touch/Bitmap.Touch.cs
+++ System.Drawing/_Touch/Bitmap.Touch.cs
@@ -74,8 +74,8 @@ namespace System.Drawing
 			case PixelFormat.Format32bppPArgb:
 			case PixelFormat.Format64bppArgb:
 			case PixelFormat.Format64bppPArgb:
-			case PixelFormat.ProprietaryFormat32bppRgba:
-			case PixelFormat.ProprietaryFormat32bppPRgba:
+			case PixelFormat.Format32bppRgbaProprietary:
+			case PixelFormat.Format32bppPRgbaProprietary:
 				return bitmap.Clone (PixelFormat.Format32bppPArgb);
 			case PixelFormat.Format1bppIndexed:
 				return bitmap.Clone (PixelFormat.Format32bppPArgb); // TODO Change to Format1bppIndexed
@@ -149,9 +149,9 @@ namespace System.Drawing
 						return PixelFormat.Format32bppPArgb;
 					case CGImageAlphaInfo.First:
 					case CGImageAlphaInfo.NoneSkipFirst:
-						return PixelFormat.ProprietaryFormat32bppRgba;
+						return PixelFormat.Format32bppRgbaProprietary;
 					case CGImageAlphaInfo.PremultipliedFirst:
-						return PixelFormat.ProprietaryFormat32bppPRgba;
+						return PixelFormat.Format32bppPRgbaProprietary;
 					default:
 						throw new ArgumentException (unsupportedText);
 					}
