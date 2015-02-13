c82d50ee68a2890eb4773b8f938840b23ca2f906
Anders Gustafsson <anders@cureos.com>
2015-02-13 13:16:03 +0100

    Separate build configuration for non-evaluation version

--- System.Drawing/_Touch/Bitmap.Touch.cs
+++ System.Drawing/_Touch/Bitmap.Touch.cs
@@ -18,7 +18,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with Shim.NET.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define EVALUATION
 
 namespace System.Drawing
 {
