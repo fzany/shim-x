20a500800e6f3b44b91470caef9ec79f3c3109b8
Anders Gustafsson <anders@cureos.com>
2015-05-13 19:59:07 +0200

    Include projects from aforge repository in build

--- Setup/Compile.cmd
+++ Setup/Compile.cmd
@@ -13,7 +13,9 @@ timeout /T 5
 @if "%VS120COMNTOOLS%"=="" goto error_no_VS120COMNTOOLSDIR
 @call "%VS120COMNTOOLS%VsDevCmd.bat"
 
-@cd ..
+@cd "..\..\aforge\Sources"
+@msbuild "Shim Drawing.sln" /t:Rebuild /p:Configuration=Release;Platform="Any CPU"
+@cd "..\..\aforge-x"
 @msbuild "Xamarin Build All.sln" /t:Rebuild /p:Configuration=Release;Platform="Any CPU"
 @msbuild "Xamarin Build All.sln" /t:Rebuild /p:Configuration="Release (Full)";Platform="Any CPU"
 
@@ -21,6 +23,8 @@ timeout /T 5
 
 @set PUBLISH=.\Setup\Publish\
 @set COMMERCIAL=.\Setup\Publish Commercial\
+@set PCLDIR=%PUBLISH%lib\portable-net45+netcore45+wpa81\
+@set WPFDIR=%PUBLISH%lib\net45\
 @set DROIDDIR=%PUBLISH%lib\MonoAndroid1\
 @set TOUCHDIR=%PUBLISH%lib\MonoTouch1\
 @set UNIFIEDDIR=%PUBLISH%lib\Xamarin.iOS10\
@@ -32,11 +36,17 @@ timeout /T 5
 
 @if EXIST "%PUBLISH%" (rd /s /q "%PUBLISH%")
 
-@md "%TOUCHDIR%"
-@xcopy /k /r /v /y /exclude:exclude.txt System.Drawing\_Touch\bin\Release\Shim.Drawing.* "%TOUCHDIR%"
+@md "%PCLDIR%"
+@xcopy /k /r /v /y /exclude:exclude.txt ..\aforge\Sources\System.Drawing\bin\Release\Shim.Drawing.* "%PCLDIR%"
+
+@md "%WPFDIR%"
+@xcopy /k /r /v /y /exclude:exclude.txt ..\aforge\Sources\System.Drawing\_WPF\bin\Release\Shim.Drawing.* "%WPFDIR%"
 
 @md "%DROIDDIR%"
 @xcopy /k /r /v /y /exclude:exclude.txt System.Drawing\_Droid\bin\Release\Shim.Drawing.* "%DROIDDIR%"
+
+@md "%TOUCHDIR%"
+@xcopy /k /r /v /y /exclude:exclude.txt System.Drawing\_Touch\bin\Release\Shim.Drawing.* "%TOUCHDIR%"
 
 @md "%UNIFIEDDIR%"
 @xcopy /k /r /v /y /exclude:exclude.txt System.Drawing\_Unified\bin\Release\Shim.Drawing.* "%UNIFIEDDIR%"
